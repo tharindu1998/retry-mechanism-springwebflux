df5b6d02dc6f6da073cfcf4ddefd2099905b20ff
tharindu1998 <tharindudulshanfdo@gmail.com>
2024-02-26 14:16:34 +0530

    New : Initial Commit for Retry Mechanism Example

--- src/main/java/com/example/retrymechanism/dto/Item.java
+++ src/main/java/com/example/retrymechanism/dto/Item.java
@@ -1,4 +1,30 @@
 package com.example.retrymechanism.dto;
 
 public class Item {
+    private String itemId;
+    private String itemName;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemId='" + itemId + '\'' +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
 }
